98b3050bc804316ddaddb3d65ae247960d4707bb
Jonathan <Jono120@users.noreply.github.com>
2018-02-16 16:27:46 +1300

    InitalCommit

---
+++ helloXamarin/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+using Android.App; 
+using Android.Content; 
+using Android.Runtime; 
+using Android.Views; 
+using Android.Widget; 
+using Android.OS;
+using helloXamarin;
+
+namespace HelloXamarin { 
+    [Activity (MainLauncher = true)]
+    public class MainActivity : Activity { 
+        protected override void OnCreate(Bundle bundle) { 
+            base.OnCreate(bundle); 
+            SetContentView(Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            button.Click += delegate
+            {
+                button.Text = "Hello world I am your first App!";
+
+            };
+        } 
+    } 
+}
